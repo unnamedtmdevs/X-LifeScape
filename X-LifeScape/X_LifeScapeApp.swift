a758a371816468ff04338dcbe5cbbee32f45c0af
ludik <bakhanetssimon@gmail.com>
2025-12-18 19:43:03 +0700

    Initial Commit

---
+++ X-LifeScape/X_LifeScapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  X_LifeScapeApp.swift
+//  X-LifeScape
+//
+//  Created by Simon Bakhanets on 18.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct X_LifeScapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ X-LifeScapeTests/X_LifeScapeTests.swift
@@ -0,0 +1,16 @@
+//
+//  X_LifeScapeTests.swift
+//  X-LifeScapeTests
+//
+//  Created by Simon Bakhanets on 18.12.2025.
+//
+
+import Testing
+
+struct X_LifeScapeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
